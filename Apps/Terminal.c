d3447adbd50b81afcc190d629290137e11ffb05f
X_141 <X_141@protonmail.com>
2020-09-11 16:53:42 -0400

    Modified terminal application. Fixed issue where first initial printout prints nothing. Cause was junk 0's passed on retrieval. Added backspace functionality to remove characters from the buffer. However, there might be a better implementation.

--- Apps/Terminal.c
+++ Apps/Terminal.c
@@ -3,32 +3,22 @@
 
 #include "string.h"
 
-void init_input_buffer(struct input_buffer* buffer) {
+// \n == 10
+// \r == 13
+// backspace is 127
+// When we hit the enter key three values are entered 13 13 10
+
+void _init_input_buffer(struct input_buffer* buffer) {
     for(int x = 0; x < MAX_INPUT_BUFFER; x++)
         buffer->_buffer[x] = '\0';
     buffer->_buffer_size = 0;
-}
-
-void init_terminal() {
-    struct input_buffer buff;
-    init_input_buffer(&buff);
-
-    enum terminal_status state = cNormal;
-
-    uart_send_string("> ");
-    while(state == cNormal) {
-        char recv_val = uart_recv();
-        state = _append_to_buffer(&buff, recv_val);
-        state = _check_buffer(&buff);
-    }
-    init_input_buffer(&buff);
 }
 
 enum terminal_status 
 _append_to_buffer(struct input_buffer* buffer, char value) {
     // We need enough space to include \r\n\0
     if(buffer->_buffer_size + 3 == MAX_INPUT_BUFFER)
-        init_input_buffer(buffer);
+        _init_input_buffer(buffer);
     buffer->_buffer[buffer->_buffer_size++] = value;
     // echo value back.
     uart_send(value);
@@ -37,16 +27,57 @@ _append_to_buffer(struct input_buffer* buffer, char value) {
 
 enum terminal_status 
 _check_buffer(struct input_buffer* buffer) {
-    // \n == 10
-    // \r == 13
-    // backspace is 127
-    // When we hit the enter key three values are entered 13 13 10
     if(buffer->_buffer[buffer->_buffer_size-1] == '\r') {
         _append_to_buffer(buffer, '\n');
         _append_to_buffer(buffer, '\0');
         uart_send_string(buffer->_buffer);
-        init_input_buffer(buffer);
+        _init_input_buffer(buffer);
         uart_send_string("> ");
     }
     return cNormal;
 }
+
+enum terminal_status
+_remove_from_buffer(struct input_buffer* buffer) {
+    if(buffer->_buffer_size != 0)
+        buffer->_buffer[--buffer->_buffer_size] = '\0';
+        
+    return cNormal;
+}
+
+void init_terminal() {
+    uart_send_string("Initializing Terminal\r\n");
+
+    struct input_buffer buff;
+    _init_input_buffer(&buff);
+
+    enum terminal_status state = cNormal;
+
+    uart_send_string("Welcome to SPKernel!\r\n");
+    uart_send_string("> ");
+    while(state == cNormal) {
+        char recv_val = uart_recv();
+        
+        // Any 0's passed will be ignored
+        if(recv_val == 0)
+            continue;
+
+        // We need to go ahead and remove a character if we can
+        if(recv_val == 127 && buff._buffer_size != 0) {
+            // We replace entire line including the term prompt
+            uart_send('\r'); 
+            for(int x = 0; x <= buff._buffer_size+2; x++)
+                uart_send(' ');
+            uart_send('\r');
+            _remove_from_buffer(&buff);
+            // start from beginning, reprint line
+            uart_send_string("> ");
+            uart_send_string(buff._buffer);
+            continue;
+        }
+
+        state = _append_to_buffer(&buff, recv_val);
+        state = _check_buffer(&buff);
+    }
+}
+
--- Apps/Terminal.h
+++ Apps/Terminal.h
@@ -17,8 +17,13 @@ enum terminal_status {
 
 void init_terminal();
 
-enum terminal_status _append_to_buffer(struct input_buffer* buffer, char value);
+enum terminal_status 
+_append_to_buffer(struct input_buffer* buffer, char value);
 
-enum terminal_status _check_buffer(struct input_buffer* buffer);
+enum terminal_status 
+_check_buffer(struct input_buffer* buffer);
+
+enum terminal_status 
+_remove_from_buffer(struct input_buffer* buffer);
 
 #endif
