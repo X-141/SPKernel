3048fa6c900a67f9ea4c4e236f2fe9032caa27c5
X_141 <X_141@protonmail.com>
2020-10-12 22:25:51 -0400

    Modified previous implementation for uint_to_string, the newer implementation should be far faster for larger values of uints. Also added strcmp for comparing two null terminated strings. the strcmp functino should function much like the standard strcmp function you find normally in string.h.

--- include/string.h
+++ include/string.h
@@ -1,6 +1,24 @@
 #ifndef _STRING_H
 #define _STRING_H
 
-int uint_to_string(char* str, unsigned int value, int size);
+/*
+ * Converts given value to ascii string and stores it into str.
+ * it in str.
+ * size parameter will limit how much of the converted value can
+ * be stored in str.
+ * 
+ * Return 0 when value can't entirely fit into str.
+ * Return 1 on success.
+*/
+int uint_to_string(char* s, unsigned int value, int size);
+
+/*
+ * Compare const char string s1 and s2
+ * 
+ * Returns 0 if s1 and s2 are equal.
+ * Return positive value if character in s1 is greater than s2.
+ * Return negative value if character in s1 is less than s2.
+*/
+int strcmp(const char* s1, const char *s2);
 
 #endif
--- src/string.c
+++ src/string.c
@@ -1,22 +1,38 @@
 #include "string.h"
 
-int uint_to_string(char* str, unsigned int value, int size) {
-    int mod = 1;
-    while(value / (mod * 10) != 0)
-        mod *= 10;
+int uint_to_string(char* s, unsigned int value, int size) {
+    int index = 0;
     
-    /* Break out if we exceed size or mod becomes 0. */
-    int x;
-    for(x = 0; x < (size - 3) && mod != 0; x++) {
-        str[x] = (value / mod) + 0x30;
-        value %= mod;
-        mod /= 10;
-    }
+    do {
+        s[index++] = (value % 10) + 0x30;
+        value /= 10;
+    } while(index < (size-3) && value != 0);
 
-    str[x++] = '\n';
-    str[x++] = '\r';
-    str[x]   = '\0';
-    if(value != 0)
-        return 0;
-    return 1;
+    // Pad last three spaces of the string with terminal 
+    // characters
+    s[index]    = '\n';
+    s[index+1]  = '\r';
+    s[index+2]  = '\0';
+    index--;
+    // Reverse
+    int reverse_index = 0;
+    char swap_char = '\0';
+    while(reverse_index < index) {
+        swap_char = s[reverse_index];
+        s[reverse_index] = s[index];
+        s[index] = swap_char;
+        
+        reverse_index++;
+        index--;
+    }
+}
+
+int strcmp(const char* s1, const char* s2) {
+    while(*s1 != '\0') {
+        if(*s1 != *s2)
+            break;
+        s1++;
+        s2++;
+    }
+    return *s1 - *s2;
 }
